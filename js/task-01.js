0f48bef9c71415c997ee6754e38d84b91be92cbe
SerhiiKutovyi <wagenrex@gmail.com>
2022-12-03 12:15:07 +0200

    Настройки

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+// const categoriesRef = document.querySelectorAll('.item');
+// console.log(`Number of categories: ${categoriesRef.length}`);
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = body.firstElementChild;
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const listItems = list.children;
+console.log(listItems);
