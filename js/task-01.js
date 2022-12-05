3f863d9d8d5c7556ec441bb3bab4aab2b349d2be
SerhiiKutovyi <wagenrex@gmail.com>
2022-12-06 00:36:03 +0200

    ДЗ-2

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,18 @@
-// const categoriesRef = document.querySelectorAll('.item');
-// console.log(`Number of categories: ${categoriesRef.length}`);
+const categoriesRef = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesRef.length}`);
 
-const totalAmount = document.querySelectorAll('h2');
-totalAmount.textContent.map(cont) => cont
-    console.log(cont);
-;
+// ======================= 1 Вариант =====================
 
+const totalAmount = document.querySelectorAll('.item ul');
+totalAmount.forEach(element => {
+  console.log(`Category: ${element.previousElementSibling.textContent}`);
+  console.log(`Elements: ${element.children.length}`);
+});
+
+// ======================= 2 Вариант =======================
+
+// const amountOfElementsEl = document.querySelectorAll('.item');
+// amountOfElementsEl.forEach(element => {
+//   console.log('Category :', element.querySelector('h2').textContent);
+//   console.log(`Elements : ${element.querySelectorAll('li').length}`);
+// });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,26 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const IngredientsRef = document.querySelector('#ingredients');
+
+// const newElement = ingredients.map(ele => {
+//   const listEl = document.createElement('li');
+//   listEl.classList.add('item');
+//   listEl.textContent = ele;
+//   return listEl;
+// });
+
+// IngredientsRef.append(...newElement);
+
+// ======================== Функция ============================ //
+
+const newIngredients = element => {
+  return element.map(ele => {
+    const listEl = document.createElement('li');
+    listEl.classList.add('item');
+    listEl.textContent = ele;
+    return listEl;
+  });
+};
+
+const newElement = newIngredients(ingredients);
+IngredientsRef.append(...newElement);
